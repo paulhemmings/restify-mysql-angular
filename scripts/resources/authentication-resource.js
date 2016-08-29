fedaadc4438936eeca3929359a40daa12f42518a
paul hemmings <phemmings@tacitknowledge.com>
2016-08-28 21:08:43 -0700

    start adding salesforce authentication

---
+++ scripts/resources/authentication-resource.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.initialize = function(server, services) {
+
+    // grab the services we need
+
+    var salesforceService = services.SalesforceService;
+
+    server.get('/login', function(req, res, next) {
+        salesforceService.login(req.username, req.password).then(function(userInfo) {
+            res.send(200, userInfo);
+            next();
+        }, function(error) {
+            res.send(301, { 'error': error});
+            next();
+        });
+    });
+
+};
---
+++ scripts/services/salesforce-service.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* https://www.npmjs.com/package/node-salesforce */
+
+(function(exports) {
+
+    var sf = require('node-salesforce');
+    var Promise = require('node-promise').Promise;
+    var conn = new sf.Connection({
+      // you can change loginUrl to connect to sandbox or prerelease env.
+      loginUrl : 'https://test.salesforce.com'
+    });
+
+    exports.name = 'SalesforceService';
+
+    exports.initialize() {
+    };
+
+    exports.login = function(username, password) {
+        var promise = new Promise();
+        conn.login(username, password, function(err, userInfo) {
+          if (err) {
+              promise.reject ('invalid token: ' + err);
+              return promise;
+          }
+          promise.resolve ({
+            accessToken : conn.accessToken,
+            instanceUrl : conn.instanceUrl,
+            userId : userInfo.id,
+            organizationId : userInfo.organizationId
+          });
+        });
+        return promise;
+    };
+
+})(exports);
